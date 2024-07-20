5751605261adc958a37ff46517ee3202d1360349
athithyaramaa1 <athithya.ramma@kalvium.community>
2024-07-20 12:52:58 +0530

    Added card component

---
+++ Cardcompo/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import BioCard from "./CardComponent";
+
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Card Component</h1>
+      <BioCard />
+    </>
+  );
+}
+
+export default App;
